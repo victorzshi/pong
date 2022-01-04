ae8accd2ed9faf26c6802f12dcd00c6a8f73dbb5
Victor Z. Shi <vzshi@pm.me>
2022-01-04 18:23:03 -0500

    Show window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,110 @@
+#include <SDL.h>
 #include <stdio.h>
+#include <string>
 
-int main()
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool start();
+
+//Frees media and shuts down SDL
+void stop();
+
+//The window we'll be rendering to
+SDL_Window* window = NULL;
+
+//The window renderer
+SDL_Renderer* renderer = NULL;
+
+bool start()
 {
-	printf("Hello World!");
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	//Set texture filtering to linear
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+	{
+		printf("Warning: Linear texture filtering not enabled!");
+	}
+
+	//Create window
+	window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+	{
+		printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	//Create vsynced renderer for window
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL)
+	{
+		printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	//Initialize renderer color
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+
+	return true;
+}
+
+void stop()
+{
+	//Destroy window	
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	window = NULL;
+	renderer = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	//Start up SDL and create window
+	if (!start())
+	{
+		printf("Failed to start!\n");
+		return 1;
+	}
+
+	//Main loop flag
+	bool running = true;
+
+	//Event handler
+	SDL_Event event;
+
+	//While application is running
+	while (running)
+	{
+		//Handle events on queue
+		while (SDL_PollEvent(&event) != 0)
+		{
+			//User requests quit
+			if (event.type == SDL_QUIT)
+			{
+				running = false;
+			}
+		}
+
+		//Clear screen
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+		SDL_RenderClear(renderer);
+
+		//Update screen
+		SDL_RenderPresent(renderer);
+	}
+
+	//Free resources and close SDL
+	stop();
 
 	return 0;
 }
